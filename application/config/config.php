6c42d7dc1b7babe44d80b48dfcd7e01d189ad642
danhb <zerocrox@gmail.com>
2020-01-24 08:53:15 -0500

    Added encryption key

--- application/config/config.php
+++ application/config/config.php
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'A&h_0*908&(*&sdf0ssdf87KJHd9f8sdlM"sKS88&*&*';
 
 /*
 |--------------------------------------------------------------------------
